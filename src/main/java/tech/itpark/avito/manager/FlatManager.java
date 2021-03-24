dcbf43da8f6125b6b82f1bb0e6f5a85489c471d8
Anastasia <nastenaprokopeva78@gmail.com>
2021-03-24 20:18:58 +0300

    fixed #1

--- src/main/java/tech/itpark/avito/manager/FlatManager.java
+++ src/main/java/tech/itpark/avito/manager/FlatManager.java
@@ -12,6 +12,33 @@ public class FlatManager {
     private long nextId = 1;
     private ArrayList<Flat> flats = new ArrayList<>();
 
+    public ArrayList<Flat> getAll() {
+
+        return flats;
+    }
+
+
+    public Flat getById(long id) {
+        for (Flat flat : flats) {
+            if (flat.getId() == id) {
+                return flat;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Flat> searchByPrice(int minPrice, int maxPrice) {
+        ArrayList<Flat> search = new ArrayList<>();
+        for (Flat flat : flats) {
+            if (flat.getPrice() >= minPrice &&
+                    flat.getPrice() <= maxPrice
+            ) {
+                search.add(flat);
+            }
+        }
+        return search;
+    }
+
     public Flat register(FlatCreate dto) {
         Flat flat = new Flat(
                 nextId++,
@@ -32,20 +59,6 @@ public class FlatManager {
         );
         flats.add(flat);
         return flat;
-    }
-
-    public ArrayList<Flat> getAll() {
-        return flats;
-    }
-
-
-    public Flat getById(long id) {
-        for (Flat flat : flats) {
-            if (flat.getId() == id) {
-                return flat;
-            }
-        }
-    return null;
     }
 
     public Flat updateById(long id, FlatUpdate dto) {
@@ -70,18 +83,6 @@ public class FlatManager {
                 flat.setRemoved(true);
             }
         }
-    }
-
-    public ArrayList<Flat> searchByPrice(int minPrice, int maxPrice) {
-        ArrayList<Flat> search = new ArrayList<>();
-        for (Flat flat : flats) {
-            if (flat.getPrice() >= minPrice &&
-                    flat.getPrice() <= maxPrice
-            ) {
-                search.add(flat);
-            }
-        }
-        return search;
     }
 }
 
